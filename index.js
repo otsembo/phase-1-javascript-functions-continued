068a9a3f0ecd8be9a89c113909ea093ab3acee30
otsembo <okumu.otsembo@gmail.com>
2022-02-28 14:32:47 +0300

    commit tree 7e1ac14896325f48257d0b35ccf2d5c4ad7184fd

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(wrap){
+    switch (wrap) {
+        case '*':
+            return function (value) {
+                return `You are *${value}*!`;
+            };
+        case '||':
+            return function (value) {
+                return `You are ||${value}||!`;
+            };
+        default:
+            return function (value) {
+                return `You are *${value}*.`;
+            };
+    }
+}
